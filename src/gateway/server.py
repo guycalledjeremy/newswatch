43824067f397044ee41eb6e268fb52765fdc9fa1
JeremyUBC <yangjier@student.ubc.ca>
2024-01-10 15:41:58 -0800

    update testing script in gateway

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -19,5 +19,28 @@ def login():
     else:
         return None, (response.text, response.status_code)
 
+def validate(jwt):
+    """placeholder for old test code.
+    """
+    headers = {
+        "Authorization": f"Bearer {jwt}",
+        "Content-Type": "application/json",
+    }
+
+    # post auth info to login function from auth service, if successful, response
+    # should contain an encoded jwt.
+    response = requests.post(
+        "http://localhost:5000/validate",
+        headers=headers
+    )
+
+    if response.status_code == 200:
+        return response.text, None
+    else:
+        return None, (response.text, response.status_code)
+
 if __name__ == "__main__":
-    print(login())
+    jwt, err = login()
+    print(jwt, err)
+
+    print(validate(jwt))
