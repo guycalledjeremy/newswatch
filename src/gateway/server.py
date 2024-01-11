322e8e1f80cbe077798b9478fab315fc84b4a648
JeremyUBC <yangjier@student.ubc.ca>
2024-01-11 12:08:14 -0800

    add tests for subscribtion service

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -39,8 +39,54 @@ def validate(jwt):
     else:
         return None, (response.text, response.status_code)
 
-if __name__ == "__main__":
-    jwt, err = login()
-    print(jwt, err)
+def subscribe():
+    """placeholder for old test code.
+    """
+    data = {
+        'username': 'admin',
+        'keyword': 'apple'
+    }
 
-    print(validate(jwt))
+    # post auth info to login function from auth service, if successful, response
+    # should contain an encoded jwt.
+    response = requests.post(
+        "http://localhost:8000/subscribe",
+        json=data
+    )
+
+    if response.status_code == 200:
+        return response.text, None
+    else:
+        return None, (response.text, response.status_code)
+
+def unsubscribe():
+    """placeholder for old test code.
+    """
+    data = {
+        'username': 'admin',
+        'keyword': 'apple'
+        # 'keyword': 'google'
+    }
+
+    # post auth info to login function from auth service, if successful, response
+    # should contain an encoded jwt.
+    response = requests.post(
+        "http://localhost:8000/unsubscribe",
+        json=data
+    )
+
+    if response.status_code == 200:
+        return response.text, None
+    else:
+        return None, (response.text, response.status_code)
+
+if __name__ == "__main__":
+    ### Tests for auth
+    # jwt, err = login()
+    # print(jwt, err)
+
+    # print(validate(jwt))
+
+    ### Tests for subscription
+    print(subscribe())
+    print(unsubscribe())
