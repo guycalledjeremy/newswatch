16794e31285e1cc6330cb049e54c8d22acffbaad
JeremyUBC <yangjier@student.ubc.ca>
2024-01-11 11:00:44 -0800

    add delete test to subscription service

--- src/subscription/sql_svc/access.py
+++ src/subscription/sql_svc/access.py
@@ -20,5 +20,27 @@ def subscribe():
     else:
         return None, (response.text, response.status_code)
 
+def delete():
+    """placeholder for old test code.
+    """
+    data = {
+        'username': 'admin',
+        'keyword': 'apple'
+        # 'keyword': 'google'
+    }
+
+    # post auth info to login function from auth service, if successful, response
+    # should contain an encoded jwt.
+    response = requests.post(
+        "http://localhost:3060/delete/subscription",
+        json=data
+    )
+
+    if response.status_code == 200:
+        return response.text, None
+    else:
+        return None, (response.text, response.status_code)
+
 if __name__ == "__main__":
     print(subscribe())
+    print(delete())
