3154188a1f469d5fa2ab142563a4b48886a58af2
JeremyUBC <yangjier@student.ubc.ca>
2024-01-11 14:48:40 -0800

    add test module and gateway tests

---
+++ src/test/gateway_tests.py
@@ -0,0 +1,92 @@
+import requests
+
+def login():
+    """placeholder for old test code.
+    """
+    basic_auth = ("admin", "Admin123")
+    # basic_auth = ("admin", "Admin12")
+    # basic_auth = ("admi", "Admin123")
+
+    # post auth info to login function from auth service, if successful, response
+    # should contain an encoded jwt.
+    response = requests.post(
+        "http://localhost:5000/login",
+        auth=basic_auth
+    )
+
+    if response.status_code == 200:
+        return response.text, None
+    else:
+        return None, (response.text, response.status_code)
+
+def validate(jwt):
+    """placeholder for old test code.
+    """
+    headers = {
+        "Authorization": f"Bearer {jwt}",
+        "Content-Type": "application/json",
+    }
+
+    # post auth info to login function from auth service, if successful, response
+    # should contain an encoded jwt.
+    response = requests.post(
+        "http://localhost:5000/validate",
+        headers=headers
+    )
+
+    if response.status_code == 200:
+        return response.text, None
+    else:
+        return None, (response.text, response.status_code)
+
+def subscribe():
+    """placeholder for old test code.
+    """
+    data = {
+        'username': 'admin',
+        'keyword': 'apple'
+    }
+
+    # post auth info to login function from auth service, if successful, response
+    # should contain an encoded jwt.
+    response = requests.post(
+        "http://localhost:8000/subscribe",
+        json=data
+    )
+
+    if response.status_code == 200:
+        return response.text, None
+    else:
+        return None, (response.text, response.status_code)
+
+def unsubscribe():
+    """placeholder for old test code.
+    """
+    data = {
+        'username': 'admin',
+        'keyword': 'apple'
+        # 'keyword': 'google'
+    }
+
+    # post auth info to login function from auth service, if successful, response
+    # should contain an encoded jwt.
+    response = requests.post(
+        "http://localhost:8000/unsubscribe",
+        json=data
+    )
+
+    if response.status_code == 200:
+        return response.text, None
+    else:
+        return None, (response.text, response.status_code)
+
+if __name__ == "__main__":
+    ### Tests for auth
+    jwt, err = login()
+    print(jwt, err)
+
+    # print(validate(jwt))
+
+    ### Tests for subscription
+    # print(subscribe())
+    # print(unsubscribe())
