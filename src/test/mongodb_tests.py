5ba2889bea1483e4e57949cdd066c3f584305799
JeremyUBC <yangjier@student.ubc.ca>
2024-01-18 12:11:37 -0800

    add test for mongodb lookup

--- src/test/mongodb_tests.py
+++ src/test/mongodb_tests.py
@@ -20,5 +20,19 @@ def add_news():
     else:
         return None, (response.text, response.status_code)
 
+def lookup_news(keyword):
+    """
+    """
+    response = requests.post(
+        f"http://localhost:9000/lookup/{keyword}"
+    )
+
+    if response.status_code == 200:
+        return response.text, None
+    else:
+        return None, (response.text, response.status_code)
+
 if __name__ == "__main__":
-    print(add_news())
+    # print(add_news())
+    print(lookup_news('apple'))
+    print(lookup_news('google'))
