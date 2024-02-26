b2d50768b762ee565cfaf8badb1834a90f9b76ee
JeremyUBC <yangjier@student.ubc.ca>
2024-02-26 10:50:34 -0800

    implement extarction service

--- src/extraction/server.py
+++ src/extraction/server.py
@@ -7,7 +7,9 @@ import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from common import config_utils
-from core_nlp import rule_based
+from core_nlp.rule_based import RuleBasedModel
+from core_nlp.gpt import GPTModel
+from .linguistics import preprocess
 from .news import extract
 from .sql_svc import query
 from .mongo_svc import access
@@ -17,11 +19,11 @@ config = config_utils.load_config("extraction/config.yaml")
 # secret config
 secret = config_utils.load_config("extraction/secret.yaml")
 
-def main(config, search_category="business", search_country="us"):
+def main(nlp_method: str, search_category: str="business", search_country: str="us"):
     """The function that extracts news and processes news before sending it to mongodb for storage.
 
     Args:
-        config: dictionary with config information.
+        nlp_method: A string that indicates the core nlp method type. Possible option: 'rule-based', 'gpt'.
         search_category: The category you want to get headlines for. Possible options: business, entertainment,
         general, health, science, sports, technology.
         search_country: The 2-letter ISO 3166-1 code of the country you want to get headlines for. e.g. 'us'.
@@ -29,26 +31,47 @@ def main(config, search_category="business", search_country="us"):
     Returns:
         A tuple, A result/error message and A status code.
     """
+    if nlp_method == 'rule-based':
+        model = RuleBasedModel()
+    elif nlp_method == 'gpt':
+        model = GPTModel()
+    else:
+        return f"Incorrect core nlp method: {nlp_method}", 404
+
     # Fetch keywords from sql db for all usernames
     query_res, err = query.get_keywords(config)
+    print(query_res)
     
     if not err:
         for keyword in query_res:
+            data = {
+                'keyword': keyword
+            }
+
             # Extract news given keywords
             crawler = extract.activate_crawler(secret["NEWS_API_KEY"])
             search_result = extract.search_news(crawler, keyword, search_category, search_country)
-            raw_articles = extract.get_articles(search_result)
+            if search_result['totalResults'] != 0:
+                all_articles = extract.get_articles(search_result)
 
-            # Use core nlp to generate report
+                # Iterate through all extracted articles
+                for article in all_articles:
+                    # Preprocess raw texts
+                    article['paragraphs'] = preprocess.filter_paragraphs(
+                        preprocess.get_paragraphs(article["text"]), keyword
+                    )
+                    
+                    # Use core nlp to generate report
+                    extracted_paragraphs = "\n".join(article['paragraphs'])
+                    article['events'] = model.find_events(extracted_paragraphs, keyword)
 
-            # Store report to mongo db
-            raw_data = {
-                'keyword': keyword,
-                'news': raw_articles
-            }
-            _, err = access.store(config, raw_data)
-            if err:
-                return err
+                # Store report to mongo db
+                data['news'] = all_articles
+                _, err = access.store(config, data)
+                if err:
+                    return err
+            else:
+                print(f"no article found for keyword: {keyword}")
     else:
         return err
 
@@ -60,5 +83,6 @@ if __name__ == "__main__":
     # scheduler.add_job(main, 'interval', minutes=1)
     # scheduler.start()
 
-    print(main(config))
-    # print(query.get_keywords(config))
+    # nlp_method = 'rule-based'
+    nlp_method = 'gpt'
+    print(main(nlp_method))
