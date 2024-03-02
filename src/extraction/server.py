c255058ec1b37c1a31aa6509d319680774f523ea
JeremyUBC <yangjier@student.ubc.ca>
2024-03-02 15:09:24 -0800

    dockerize extraction service

--- src/extraction/server.py
+++ src/extraction/server.py
@@ -1,36 +1,44 @@
 """This is the module that extracts news and stores in mongodb through api calls.
 """
 
+import atexit
 import datetime
+import os
 
-# from flask import Flask, request
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask
 
-from common import config_utils
 from core_nlp.rule_based import RuleBasedModel
 from core_nlp.gpt import GPTModel
-from .linguistics import preprocess
-from .news import extract
-from .sql_svc import query
-from .mongo_svc import access
+from linguistics import preprocess
+from news import extract
+from sql_svc import query
+from mongo_svc import access
+
+# configures Flask server and mysql database
+server = Flask(__name__)
 
 # config
-config = config_utils.load_config("extraction/config.yaml")
-# secret config
-secret = config_utils.load_config("extraction/secret.yaml")
+config = {
+    "SQL_SVC_ADDRESS": os.environ.get("SQL_SVC_ADDRESS"),
+    "MONGO_SVC_ADDRESS": os.environ.get("MONGO_SVC_ADDRESS")
+}
 
-def main(nlp_method: str, search_category: str="business", search_country: str="us"):
+def main():
     """The function that extracts news and processes news before sending it to mongodb for storage.
-
-    Args:
-        nlp_method: A string that indicates the core nlp method type. Possible option: 'rule-based', 'gpt'.
-        search_category: The category you want to get headlines for. Possible options: business, entertainment,
-        general, health, science, sports, technology.
-        search_country: The 2-letter ISO 3166-1 code of the country you want to get headlines for. e.g. 'us'.
 
     Returns:
         A tuple, A result/error message and A status code.
     """
+    # fetch parameters from yaml files
+    # nlp_method: A string that indicates the core nlp method type. Possible option: 'rule-based', 'gpt'.
+    nlp_method = os.environ.get("NLP_METHOD")
+    # search_category: The category you want to get headlines for. Possible options: business, entertainment,
+    # general, health, science, sports, technology.
+    search_category = os.environ.get("SEARCH_CATEGORY")
+    # search_country: The 2-letter ISO 3166-1 code of the country you want to get headlines for. e.g. 'us'.
+    search_country = os.environ.get("SEARCH_COUNTRY")
+
     if nlp_method == 'rule-based':
         model = RuleBasedModel()
     elif nlp_method == 'gpt':
@@ -49,7 +57,7 @@ def main(nlp_method: str, search_category: str="business", search_country: str="
             }
 
             # Extract news given keywords
-            crawler = extract.activate_crawler(secret["NEWS_API_KEY"])
+            crawler = extract.activate_crawler(os.environ.get("NEWS_API_KEY"))
             search_result = extract.search_news(crawler, keyword, search_category, search_country)
             if search_result['totalResults'] != 0:
                 all_articles = extract.get_articles(search_result)
@@ -77,12 +85,30 @@ def main(nlp_method: str, search_category: str="business", search_country: str="
 
     return f'successful news extraction, timestamp: {datetime.datetime.utcnow()}', 200
 
+def test():
+    print('yes!')
+
+# initiates aps scheduler
+scheduler = BackgroundScheduler(daemon=True)
+# add main function to scheduler and start scheduler
+scheduler.add_job(func=test, trigger='interval', seconds=30)
+scheduler.start()
+
+# shut down the scheduler when exiting the flask server
+# atexit.register(lambda: scheduler.shutdown())
+
 if __name__ == "__main__":
-    # server.run(host="0.0.0.0", port=9050, debug=True)
+    server.run(host="0.0.0.0", port=9050, debug=True)
     # scheduler = BlockingScheduler()
     # scheduler.add_job(main, 'interval', minutes=1)
     # scheduler.start()
 
+    # # initiates aps scheduler
+    # scheduler = BackgroundScheduler(daemon=True)
+    # # add main function to scheduler and start scheduler
+    # scheduler.add_job(func=test, trigger='interval', seconds=15)
+    # scheduler.start()
+
     # nlp_method = 'rule-based'
-    nlp_method = 'gpt'
-    print(main(nlp_method))
+    # nlp_method = 'gpt'
+    # print(main(nlp_method))
